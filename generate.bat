aecfa594985be0f0a2b6af08b2176701efe46da3
Andy <suharev.andrey@gmail.com>
2025-06-18 11:42:35 +0300

    Update output directory in generate.bat for AsyncAPI documentation generation

--- generate.bat
+++ generate.bat
@@ -1,1 +1,1 @@
-asyncapi generate fromTemplate ./api/asyncapi/spec.yaml @asyncapi/html-template@3.0.0 -o ./api/asyncapi/doc --use-new-generator
+asyncapi generate fromTemplate ./api/asyncapi/spec.yaml @asyncapi/html-template@3.0.0 -o ./doc --use-new-generator
